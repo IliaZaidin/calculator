708126f2310a5c46827f820b8d2d289454ee64a4
iliazaidin <ilia.zaid@gmail.com>
2022-02-06 10:47:05 +0200

    fix/math logic

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import logo from '../images/logo.jpg';
 
 function App() {
-  const [inputData, setInputData] = React.useState('');
+  const [inputData, setInputData] = React.useState([]);
   const [historyData, setHistoryData] = React.useState('');
+  const [displayData, setDisplayData] = React.useState('');
 
-  function doMath(str) {
-    let arr = str.split(' ');
-
+  function doMath() {
+    let arr = inputData;
     for (let i = 0; i < arr.length; i++) {
       if (arr[i] === '*' || arr[i] === '/') {
         if (arr[i] === '*') {
@@ -23,48 +23,58 @@ function App() {
     }
 
     for (let i = 0; i < arr.length; i++) {
-      if (arr[i] === '+') {
-        arr[i - 1] = parseFloat(arr[i - 1]) + parseFloat(arr[i + 1]);
-        arr.splice(i, 2);
-        i--;
+      if (arr[i] === '+' || arr[i] === '-') {
+        if (arr[i] === '+') {
+          arr[i - 1] = parseFloat(arr[i - 1]) + parseFloat(arr[i + 1]);
+          arr.splice(i, 2);
+          i--;
+        } else {
+          arr[i - 1] = parseFloat(arr[i - 1]) - parseFloat(arr[i + 1]);
+          arr.splice(i, 2);
+          i--;
+        }
       }
     }
-
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] === '-') {
-        arr[i - 1] = parseFloat(arr[i - 1]) - parseFloat(arr[i + 1]);
-        arr.splice(i, 2);
-        i--;
-      }
-    }
-    return arr[0];
+    setInputData(arr[0]);
   }
 
   function handleKeypadClick(event) {
     if (event.target.innerText === '=') {
-      const result = doMath(inputData);
-      setInputData('');
-      setHistoryData(result);
-    }
-    else if (event.target.innerText === '+' || event.target.innerText === '-' || event.target.innerText === '*' || event.target.innerText === '/') {
-      if (inputData[inputData.length - 2] === '+' || inputData[inputData.length - 2] === '-' || inputData[inputData.length - 2] === '*' || inputData[inputData.length - 2] === '/') {
-        const temp = inputData.slice(0, -3);
+      doMath();
+      setHistoryData(inputData);
+      setInputData([]);
+      setDisplayData('');
+    } else {
+      const temp = inputData;
+      if (event.target.innerText === '+' || event.target.innerText === '-' || event.target.innerText === '*' || event.target.innerText === '/') {
+        temp.push(event.target.innerText);
         setInputData(temp);
+        setDisplayData(inputData.join(''));
+      } else {
+        if (inputData.length === 0 || inputData[inputData.length - 1] === '+' || inputData[inputData.length - 1] === '-' || inputData[inputData.length - 1] === '*' || inputData[inputData.length - 1] === '/') {
+          temp.push(event.target.innerText);
+          setInputData(temp);
+          setDisplayData(inputData.join(''));
+        } else {
+          temp[temp.length - 1] += event.target.innerText;
+          setInputData(temp);
+          setDisplayData(inputData.join(''));
+        }
       }
-      setInputData(inputData.concat(' ' + event.target.innerText + ' '));
     }
-    else
-      setInputData(inputData.concat(event.target.innerText));
   }
 
   function handleHistoryClick() {
-    if (inputData === '' || inputData[inputData.length - 1] === ' ')
-      setInputData(inputData.concat(historyData));
+    const temp = inputData;
+    temp.push(historyData);
+    setInputData(temp);
+    setDisplayData(inputData.join(''));
   }
 
   function handleCancel() {
-    setInputData('');
+    setInputData([]);
     setHistoryData('');
+    setDisplayData('');
   }
 
   return (
@@ -77,7 +87,7 @@ function App() {
         <main className="main">
           <section className="screen">
             <button className="history" onClick={handleHistoryClick}>Result: {historyData}</button>
-            <h2 className="input">Input: {inputData}</h2>
+            <h2 className="input">Input: {displayData}</h2>
           </section>
           <section className="keypad">
             <button className="button" type="button" onClick={handleKeypadClick}>7</button>
